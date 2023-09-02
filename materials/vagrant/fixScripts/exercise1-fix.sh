6f424840bdeb41e5670e84a03b389597a89e4db5
yafit89 <137615537+yafit89@users.noreply.github.com>
2023-09-02 17:50:32 +0300

    Update exercise1-fix.sh

--- materials/vagrant/fixScripts/exercise1-fix.sh
+++ materials/vagrant/fixScripts/exercise1-fix.sh
@@ -1,2 +1,6 @@
 #!/bin/bash# package updates
 sudo yum check-update
+sudo yum update # apache installation, enabling and status check
+sudo yum -y install httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
