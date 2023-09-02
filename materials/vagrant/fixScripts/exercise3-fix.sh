b179f9b115cf6824df938b8f33247af2a8a36f93
yafit89 <137615537+yafit89@users.noreply.github.com>
2023-09-02 18:28:56 +0300

    Update exercise3-fix.sh

--- materials/vagrant/fixScripts/exercise3-fix.sh
+++ materials/vagrant/fixScripts/exercise3-fix.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#add fix to exercise3 here
 cd /etc/apache2/sites-enabled
-sudo sed -i '33s/denied/granted/g' 000-default.conf
-sudo service apache2 restart
+vi 000-default
+change to "require all granted"
+sudo systemctl restart apache2
